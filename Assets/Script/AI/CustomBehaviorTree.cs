e7be668a694df33eca37afdfc3797feb38d73c39
HyesungKum <rmagptjd77@gmail.com>
2022-11-23 21:06:54 +0900

    add card frame modeling, add backpack card object, doing make cartoon shader and studing cg& hlsl shaderlab

---
+++ Assets/Script/AI/CustomBehaviorTree.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBehaviorTree : BehaviorTree
+{
+    //말단 노드가 action node가 되어 행동 하나하나를 표현함
+    //깊이 우선탐색
+    //유지보수가 쉬움
+
+    //selector -> if/else
+    //sequence -> while
+    //Paralle -> repeat
+
+    //말단노드는 action노드가 됨
+}
